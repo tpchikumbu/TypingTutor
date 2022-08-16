024348994806b924da032e38f378dc5b50cdec6e
tpchikumbu <tpchikumbu@gmail.com>
2022-08-16 10:51:04 +0200

    creation of directory

---
+++ src/typingTutor/CatchWord.java
@@ -0,0 +1,47 @@
+package typingTutor;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+//Thread to monitor the word that has been typed.
+public class CatchWord extends Thread {
+	String target;
+	static AtomicBoolean done ; //REMOVE
+	static AtomicBoolean pause; //REMOVE
+	
+	private static  FallingWord[] words; //list of words
+	private static int noWords; //how many
+	private static Score score; //user score
+	
+	CatchWord(String typedWord) {
+		target=typedWord;
+	}
+	
+	public static void setWords(FallingWord[] wordList) {
+		words=wordList;	
+		noWords = words.length;
+	}
+	
+	public static void setScore(Score sharedScore) {
+		score=sharedScore;
+	}
+	
+	public static void setFlags(AtomicBoolean d, AtomicBoolean p) {
+		done=d;
+		pause=p;
+	}
+	
+	public void run() {
+		int i=0;
+		while (i<noWords) {		
+			while(pause.get()) {};
+			if (words[i].matchWord(target)) {
+				System.out.println( " score! '" + target); //for checking
+				score.caughtWord(target.length());	
+				//FallingWord.increaseSpeed();
+				break;
+			}
+		   i++;
+		}
+		
+	}	
+}
